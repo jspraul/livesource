1c808c90036f0fb6cf13ed680c4f18f184d19a65
nleghari <nleghari@46194eaa-a47d-11de-ac05-951101a8bf3e>
2009-09-22 13:46:45 +0000

    Removed end statement logging. Refactor code

--- src/Core/CecilModel/CodeBase.cs
+++ src/Core/CecilModel/CodeBase.cs
@@ -8,32 +8,16 @@ namespace LiveSource.Core.CecilModel
 {
     internal class CodeBase
     {
-        private readonly AssemblyDefinition _Assembly;
-
+        private readonly AssemblyDefinition TargetAssemblyDefinition;
         private readonly MethodReference loggerDebug;
-
-        public CodeBase(string assemblyFile)
-        {
-            _Assembly = AssemblyFactory.GetAssembly(assemblyFile);
-
-            MethodInfo debugWriter = typeof (LoggingAspect.AspectLogger).GetMethod("Debug", new[] {typeof (string)});
-            loggerDebug = _Assembly.MainModule.Import(debugWriter);
-        }
-
-        public void AddAssemblyReference()
-        {
-            Logger.Current.Info(">>>> Adding assembly reference to LoggingAspect");
-            AssemblyNameReference aspectAssemblyReference = new AssemblyNameReference("LoggingAspect.dll", "",
-                                                                                      new Version(1, 0, 0, 0));
-            _Assembly.MainModule.AssemblyReferences.Add(aspectAssemblyReference);
-        }
+        public readonly string AssemblyFile;
 
         public List<CodeType> Types
         {
             get
             {
                 List<CodeType> codeTypes = new List<CodeType>();
-                foreach (TypeDefinition typeDefinition in _Assembly.MainModule.Types)
+                foreach (TypeDefinition typeDefinition in TargetAssemblyDefinition.MainModule.Types)
                 {
                     codeTypes.Add(new CodeType(typeDefinition));
                 }
@@ -42,33 +26,61 @@ namespace LiveSource.Core.CecilModel
             }
         }
 
-        public void Inject(CodeMethod method)
+        public CodeBase(string assemblyFile)
+        {
+            this.AssemblyFile = assemblyFile;
+            TargetAssemblyDefinition = AssemblyFactory.GetAssembly(this.AssemblyFile);
+
+            MethodInfo debugWriter = typeof (LoggingAspect.AspectLogger).GetMethod("Debug", new[] {typeof (string)});
+            loggerDebug = TargetAssemblyDefinition.MainModule.Import(debugWriter);
+        }
+
+        public void AddAssemblyReference()
+        {
+            Logger.Current.Info(">>>> Adding assembly reference to LoggingAspect");
+            AssemblyNameReference aspectAssemblyReference = new AssemblyNameReference("LoggingAspect.dll", "",
+                                                                                      new Version(1, 0, 0, 0));
+            TargetAssemblyDefinition.MainModule.AssemblyReferences.Add(aspectAssemblyReference);
+        }
+
+        public void InjectLogStatements()
+        {
+            foreach (CodeType codeType in this.Types) 
+            {
+                foreach (CodeMethod codeMethod in codeType.Methods) 
+                {
+                    this.Inject(codeMethod);
+                }
+            }    
+        }
+
+        private void Inject(CodeMethod method)
         {
             Logger.Current.Debug("Method >> " + method.MethodDefinition.Name);
 
-            if (null == method.MethodDefinition.Body)
+            if (!ValidMethod(method))
                 return;
 
-            foreach (CustomAttribute customAttribute in method.MethodDefinition.CustomAttributes)
-            {
+            AddStartMethodStatement(method, method.MethodDefinition.Body.Instructions[0], "Begin");
+        }
+
+        private static bool ValidMethod(CodeMethod method)
+        {
+            if (null == method.MethodDefinition.Body)
+                return false;
+
+            foreach (CustomAttribute customAttribute in method.MethodDefinition.CustomAttributes) {
+                // TODO: Make following configurable
                 if (Equals(customAttribute.Constructor.DeclaringType.FullName,
                            "System.Diagnostics.DebuggerNonUserCodeAttribute") ||
                     Equals(customAttribute.Constructor.DeclaringType.FullName,
-                           "System.Runtime.CompilerServices.CompilerGeneratedAttribute"))
+                           "System.Runtime.CompilerServices.CompilerGeneratedAttribute")) 
                 {
-                    return;
+                    return false;
                 }
             }
 
-            AddStartMethodStatement(method, method.MethodDefinition.Body.Instructions[0], "Begin");
-
-            foreach (CodeInstruction i in method.Instructions)
-            {
-                if (Equals(i.Instruction.OpCode, OpCodes.Ret))
-                {
-                    AddEndMethodStatement(method, i.Instruction, "End");
-                }
-            }
+            return true;
         }
 
         private void AddStartMethodStatement(CodeMethod method, Instruction instruction, string prefix)
@@ -81,16 +93,6 @@ namespace LiveSource.Core.CecilModel
             InsertStatement(method, instruction, beginSentence);
         }
 
-        private void AddEndMethodStatement(CodeMethod method, Instruction instruction, string prefix)
-        {
-            Instruction endSentence = method.MethodDefinition.Body.CilWorker.Create(OpCodes.Ldstr,
-                                                                                    prefix + "-" +
-                                                                                    method.MethodDefinition.
-                                                                                        DeclaringType.FullName + "-" +
-                                                                                    method.MethodDefinition.Name);
-            InsertStatement(method, instruction, endSentence);
-        }
-
         private void InsertStatement(CodeMethod method, Instruction lastInstruction, Instruction endSentence)
         {
             Instruction callLoggerDebug = method.MethodDefinition.Body.CilWorker.Create(OpCodes.Call, loggerDebug);
@@ -99,9 +101,14 @@ namespace LiveSource.Core.CecilModel
             method.MethodDefinition.Body.CilWorker.InsertAfter(callLoggerDebug, method.MethodDefinition.Body.CilWorker.Create(OpCodes.Nop));
         }
 
-        public void Save(string assemblyFile)
+        private void Save(string assemblyFile)
         {
-            AssemblyFactory.SaveAssembly(_Assembly, assemblyFile);
+            AssemblyFactory.SaveAssembly(TargetAssemblyDefinition, assemblyFile);
+        }
+
+        ~CodeBase()
+        {
+            Save(this.AssemblyFile);
         }
     }
 }
