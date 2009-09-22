19b54370c8d846b374e2fbe1e1d5f21e08e07e97
nleghari <nleghari@46194eaa-a47d-11de-ac05-951101a8bf3e>
2009-09-22 13:46:45 +0000

    Removed end statement logging. Refactor code

--- trunk/src/Core/CecilModel/CecilAssemblyData.cs
+++ trunk/src/Core/CecilModel/CecilAssemblyData.cs
@@ -13,20 +13,11 @@
         {
             Logger.Current.Info("Processing assembly:" + this.AssemblyFile);
 
-            // TODO: Refactor to dispose pattern
             CodeBase codeBase = new CodeBase(this.AssemblyFile);
-
-            codeBase.AddAssemblyReference();
-//            // TODO: Refactor the following code into CodeBase. just call codeBase.Inject();
-            foreach (CodeType codeType in codeBase.Types) 
             {
-                foreach (CodeMethod codeMethod in codeType.Methods) 
-                {
-                    codeBase.Inject(codeMethod);
-                }
+                codeBase.AddAssemblyReference();
+                codeBase.InjectLogStatements();
             }
-            
-            codeBase.Save(this.AssemblyFile);
         }
     }
 }
